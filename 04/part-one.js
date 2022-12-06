2a81a15219c03a8555b1aa52a8591a2c7ffcfc21
immeyti <mehdi.samirakh68@gmail.com>
2022-12-06 11:00:24 +0000

    Day4: Camp Cleanup

---
+++ 04/part-one.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+fs.readFile('./inputs.txt', 'utf8', (err, originalData) => { // Reading the inputs
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    const pairsIdRang = originalData.split("\n"); // Removing newlines 
+    let duplicated = 0;
+
+    pairsIdRang.forEach(pair => {
+        const extractedPair = pair.split(',').reduce((accumulator, currentValue) => {
+            const ids = currentValue.split('-');
+            accumulator.push({ start: Number(ids[0]), end: Number(ids[1]) });
+            return accumulator;
+        }, []);
+        
+        const firsElv = extractedPair[0];
+        const secondElv = extractedPair[1];
+        if ((firsElv.start <= secondElv.start && firsElv.end >= secondElv.end) || 
+            (firsElv.start >= secondElv.start && firsElv.end <= secondElv.end)) {
+            duplicated += 1;
+            console.log(extractedPair);
+        }
+    });
+
+    console.log(duplicated, pairsIdRang.length);
+});
